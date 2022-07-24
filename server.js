4d5d006b358d335e4659b9c37a7d09e44bcaec46
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-23 20:00:25 -0400

    linked data and made express handle and format data

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const db = require('./db/db.json');
 var app = express();
 var PORT = process.env.PORT || 3001;
 
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
