4fad3da5821ee1be8993a6309a3fea405226f8e2
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-23 20:13:47 -0400

    added get request for the index html on initial load

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
 app.listen(PORT, function () {
     console.log('App waiting on port-' + PORT);
 })
